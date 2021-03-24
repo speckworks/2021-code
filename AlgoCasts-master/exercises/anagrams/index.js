86e4a8fe91b4d877fc5c40eb3fc9636b9b6c7532
John Speck <speckworks@gmail.com>
2021-03-23 23:03:55 -0400

    doin dis

--- AlgoCasts-master/exercises/anagrams/index.js
+++ AlgoCasts-master/exercises/anagrams/index.js
@@ -11,41 +11,71 @@
 
 function anagrams(stringA, stringB) {
 
-    firstString = stringA.toLowerCase().replace(/\W/g, '');
-    secondString = stringB.toLowerCase().replace(/\W/g, '');
-    stringA.split(' ').join('')
-    stringB.split(' ').join('')
-    console.log(firstString);
-    console.log(secondString);
-    
-    let charMap = {};
-    
-    for(let char of firstString){
-      if(!charMap[char]){
-        charMap[char] = 1;
-      } else {
-        charMap[char]++;
-      }
-    }
-    
-    for(let char of secondString){
-    if(!charMap[char]){
-      return false;
-    } else {
-     charMap[char]--;
-    } 
-    }
-    
-    for(let char in charMap){
-      if(charMap[char] == 0){
-        return true;
-      } else {
-       return false
-      }
-    }
-    
+const aCharMap = buildCharMap(stringA);
+const bCharMap = buildCharMap(stringB);
+
+if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length ){
+  return false;
+}
+
+for(let char in aCharMap)
+if(aCharMap[char] !== bCharMap[char]){
+  return false;
+} else {
+return true;
+} 
     
     }
+
+function buildCharMap(str){
+const charMap = {};
+
+for(let char of str.replace(/\W/g, '').toLowerCase()){
+  charMap[char] = charMap[char] + 1 || 1;
+}
+
+return charMap;
+
+}
+
+    // function anagrams(stringA, stringB) {
+
+    //   firstString = stringA.toLowerCase().replace(/\W/g, '');
+    //   secondString = stringB.toLowerCase().replace(/\W/g, '');
+    //   stringA.split(' ').join('')
+    //   stringB.split(' ').join('')
+    //   console.log(firstString);
+    //   console.log(secondString);
+      
+    //   let charMap = {};
+      
+    //   for(let char of firstString){
+    //     if(!charMap[char]){
+    //       charMap[char] = 1;
+    //     } else {
+    //       charMap[char]++;
+    //     }
+    //   }
+      
+    //   for(let char of secondString){
+    //   if(!charMap[char]){
+    //     return false;
+    //   } else {
+    //    charMap[char]--;
+    //   } 
+    //   }
+      
+    //   for(let char in charMap){
+    //     if(charMap[char] == 0){
+    //       return true;
+    //     } else {
+    //      return false
+    //     }
+    //   }
+      
+      
+    //   }
+  
 
 
 module.exports = anagrams;
